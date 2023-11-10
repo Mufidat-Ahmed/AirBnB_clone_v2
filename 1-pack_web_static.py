a36c6bfcebc44f7f2b2a691275aa38785a527235
Ane-A <53179280+Ane-A@users.noreply.github.com>
2023-11-10 14:03:48 +0100

    Update : 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,22 +8,22 @@ from fabric.api import local
 def do_pack():
   """This function creates a .tgz archive from the contents of the web_static folder."""
 
-  # Create the versions folder if it doesn't exist.
+  """ Create the versions folder if it doesn't exist. """
   if not os.path.isdir("versions"):
     local("mkdir versions")
 
-  # Get the current date and time.
+  """ Get the current date and time. """
   now = datetime.now()
 
-  # Generate the archive name.
+  """ Generate the archive name. """
   archive_name = "versions/web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
 
-  # Create the archive.
+  """ Create the archive. """
   local("tar -cvzf {} web_static".format(archive_name))
 
   return archive_name if os.path.isfile(archive_name) else None
 
-# Example usage:
+""" Example usage: """
 
 archive_path = do_pack()
 if archive_path is not None:
